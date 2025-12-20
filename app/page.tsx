a2b6d0b13a7e333cd518aabf32aa86b619b2adc5
jaadu611 <ayushburman281.com>
2025-12-21 04:11:28 +0530

    Implemented Dock with proper comments and updated readme.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Dock from "@/components/Dock";
 import Welcome from "@/components/Welcome";
 
 const page = () => {
   return (
     <main>
       <Welcome />
+      <Dock />
     </main>
   );
 };
---
+++ constants/Dock.constants.ts
@@ -0,0 +1,40 @@
+const dockApps = [
+  {
+    id: "finder",
+    name: "Portfolio",
+    icon: "finder.webp",
+    canOpen: true,
+  },
+  {
+    id: "safari",
+    name: "Articles",
+    icon: "safari.webp",
+    canOpen: true,
+  },
+  {
+    id: "photos",
+    name: "Gallary",
+    icon: "photos.webp",
+    canOpen: true,
+  },
+  {
+    id: "contacts",
+    name: "Contacts",
+    icon: "contact.webp",
+    canOpen: true,
+  },
+  {
+    id: "terminal",
+    name: "Skills",
+    icon: "terminal.webp",
+    canOpen: true,
+  },
+  {
+    id: "trash",
+    name: "Archive",
+    icon: "trash.webp",
+    canOpen: false,
+  },
+];
+
+export { dockApps };
