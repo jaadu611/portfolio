0efb16b9ab55753c3ed6cbbd51785f394fba5b44
jaadu611 <ayushburman281.com>
2025-12-27 21:43:24 +0530

    fixed all the conflicts and saved my code

--- constants/Navabar.constants.ts
+++ constants/Navabar.constants.ts
@@ -7,15 +7,6 @@ const useNavbarIcons = () => {
   const { toggleControlCenter } = useControlCenterStore();
 
   return [
-<<<<<<< HEAD
-    { name: "Battery", link: "/icons/Battery.svg", onClick: () => {} },
-    { name: "Wifi", link: "/icons/Wifi.svg", onClick: () => {} },
-    { name: "Search", link: "/icons/Search.svg", onClick: () => {} },
-    { name: "User", link: "/icons/User.svg", onClick: () => {} },
-    {
-      name: "Switch",
-      link: "/icons/Switch.svg",
-=======
     { name: "Battery", link: "/icons/Battery", onClick: () => {} },
     { name: "Wifi", link: "/icons/Wifi", onClick: () => {} },
     { name: "Search", link: "/icons/Search", onClick: () => {} },
@@ -23,7 +14,6 @@ const useNavbarIcons = () => {
     {
       name: "Switch",
       link: "/icons/Switch",
->>>>>>> 1fe523df935f1f8f814d7a5f8e71b31e65f6d6de
       onClick: toggleControlCenter,
     },
   ] as const;
